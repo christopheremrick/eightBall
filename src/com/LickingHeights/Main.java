69dea42e598f84d13b32fe2876e952cc4800a21e
Chris <christopher.emrick.2022@student.lhschools.org>
2019-02-21 13:53:42 -0500

    Created all messages for input number

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -9,7 +9,7 @@ public class Main {
     // 1-100
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("Hello. Please type a number between 1 and 100.");
+        System.out.println("Hello. Please type a number between 1 and 100 and think of a question.");
 
         Integer number = scan.nextInt();
 
@@ -17,46 +17,46 @@ public class Main {
     // 10 options
 
         if(number < 11) {
-
+            System.out.println("Most certainly.");
 
         }
 
         if(number < 21 && number > 11) {
-
+            System.out.println("Probably not.");
 
         }
 
         if(number < 31 && number > 21) {
-
+            System.out.println("Yeah, no.");
 
         }
 
         if(number < 41 && number > 31) {
-
+            System.out.println("Sounds amazing.");
 
         }
 
         if(number < 51 && number > 41) {
-
+            System.out.println("Yeah... I wouldn't.");
 
         }
 
         if(number < 61 && number > 51) {
-
+            System.out.println("I would stay well away from that...");
 
         }
 
         if(number < 71 && number > 61) {
-
+            System.out.println("You should probably think about something else...");
 
         }
 
         if(number < 81 && number > 71) {
-
+            System.out.println("Most likely.");
 
         }
         if(number < 91 && number > 81) {
-
+            System.out.println("Go for it!");
 
         }
     }
