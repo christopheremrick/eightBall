5c7e2733c98a3ac4f01ffdacd95cb03b20462a82
Chris <christopher.emrick.2022@student.lhschools.org>
2019-02-21 13:48:54 -0500

    Created all if statements
    Created all if statements using 2 booleans in 9 of them and 1 in the first.

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -8,6 +8,56 @@ public class Main {
 	// 5 positive, 5 negetive
     // 1-100
 
-        Scanner scan = new Scanner(System.in);    
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Hello. Please type a number between 1 and 100.");
+
+        Integer number = scan.nextInt();
+
+
+    // 10 options
+
+        if(number < 11) {
+
+
+        }
+
+        if(number < 21 && number > 11) {
+
+
+        }
+
+        if(number < 31 && number > 21) {
+
+
+        }
+
+        if(number < 41 && number > 31) {
+
+
+        }
+
+        if(number < 51 && number > 41) {
+
+
+        }
+
+        if(number < 61 && number > 51) {
+
+
+        }
+
+        if(number < 71 && number > 61) {
+
+
+        }
+
+        if(number < 81 && number > 71) {
+
+
+        }
+        if(number < 91 && number > 81) {
+
+
+        }
     }
 }
