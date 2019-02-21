9f90dfa10e26cf2a84fedc3a804cccef4ea83acd
Chris <christopher.emrick.2022@student.lhschools.org>
2019-02-21 13:59:38 -0500

    Changed conditions to else if and added extra option

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -21,49 +21,53 @@ public class Main {
 
         }
 
-        if(number < 21 && number > 11) {
+        else if(number < 21 && number > 11) {
             System.out.println("Probably not.");
 
         }
 
-        if(number < 31 && number > 21) {
+        else if(number < 31 && number > 21) {
             System.out.println("Yeah, no.");
 
         }
 
-        if(number < 41 && number > 31) {
+        else if(number < 41 && number > 31) {
             System.out.println("Sounds amazing.");
 
         }
 
-        if(number < 51 && number > 41) {
+        else if(number < 51 && number > 41) {
             System.out.println("Yeah... I wouldn't.");
 
         }
 
-        if(number < 61 && number > 51) {
+        else if(number < 61 && number > 51) {
             System.out.println("I would stay well away from that...");
 
         }
 
-        if(number < 71 && number > 61) {
+        else if(number < 71 && number > 61) {
             System.out.println("You should probably think about something else...");
 
         }
 
-        if(number < 81 && number > 71) {
+        else if(number < 81 && number > 71) {
             System.out.println("Most likely.");
 
         }
 
-        if(number < 91 && number > 81) {
+        else if(number < 91 && number > 81) {
             System.out.println("Go for it!");
 
         }
 
-        if(number < 101 && number > 91) {
+        else if(number < 101 && number > 91) {
             System.out.println("That could happen.");
 
+        }
+
+        else {
+            System.out.println("That isn't a number between 1 and 100.");
         }
     }
 }
